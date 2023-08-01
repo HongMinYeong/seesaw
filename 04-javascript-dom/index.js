ad3a84253597b7815d17cc6fb74f54d8a7da7ce0
boss <minyung1240@daum.net>
2023-08-02 01:19:44 +0900

    Review : js dom 요소 다루기

--- 04-javascript-dom/index.js
+++ 04-javascript-dom/index.js
@@ -1,4 +1,5 @@
 console.log(document);
+console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 console.log(document.title);
@@ -10,12 +11,68 @@ console.log(document.URL);
 console.log(document.getElementById('green'));
 console.log(document.getElementById('red'));
 
-// 2. getElementsByClassName
-console.log(document.getElementsByClassName('pink')); // 유사배열
+//2. getElementsByClassName -> class 이름을 기준으로 해서 가지고 오겠다.
+//class 는 여러개 지정할수 있어서 s 붙었네요 하하
+console.log('=============2. getElementsByClassName==============');
+console.log(document.getElementsByClassName('pink')); //유사배열
 console.log(document.getElementsByClassName('pink')[1]);
+////////////////////////////////////////////////////////////////////////////////
+// document.getElementsByClassName('pink').forEach((elem) => {
+//   console.log(elem);
+// });
+// //유사배열이지 배열은 아니기때문에 실행안됨
 
-// 3. getElementsByTagName
+console.log('============3.getElementsByTagName===============');
+//3.getElementsByTagName
 console.log(document.getElementsByTagName('div'));
 
-// 4. getElementsByName
+console.log('===========4.getElementsByName================');
+//4.getElementsByName
 console.log(document.getElementsByName('id'));
+
+console.log('=============5. querySelector(css selector)==============');
+//5. querySelector(css selector)
+// : 처음 발견한 하나만 가지고 옴
+console.log(document.querySelector('.pink'));
+console.log(document.querySelector('.others'));
+console.log(document.querySelector('#green'));
+console.log(document.querySelector('div'));
+console.log(document.querySelector('[name="id"]'));
+
+console.log('==============6.querySelectorAll (css selector)=============');
+//6.querySelectorAll (css selector)
+// : 선택자에 해당되는 모든 요소를 선택
+console.log(document.querySelectorAll('.pink'));
+console.log(document.querySelectorAll('.others'));
+console.log(document.querySelectorAll('#green'));
+console.log(document.querySelectorAll('div'));
+console.log(document.querySelectorAll('[name="id"]'));
+
+console.log(document.querySelectorAll('.pink')[0]);
+console.log(document.querySelectorAll('.pink')[1]);
+console.log(document.querySelectorAll('.pink')[2]);
+console.log(document.querySelectorAll('.pink')[3]);
+
+//유사 배열 (HTMLcollection, NodeList)
+//[] 식으로 생긴 배열을 의미. 배열은 아님 !!
+//index,length 는 가짐 .. 그치만 배열은 아님..!!!!
+//배열과 달리 사용 가능한 메서드가 제한
+
+//nodeList -> forEach() 반복문 사용 가능
+console.log('===========forEach ===========');
+document.querySelectorAll('.pink').forEach((elem) => console.log(elem));
+
+//HTMLCollection -> forEach() 메서드 사용 x
+//반복을 해야 된다? Array 변경 (Array.from())
+console.log('===========forEach array 변경===========');
+//array 형태로 바꾼다음 forEach 돌기
+Array.from(document.getElementsByClassName('pink')).forEach((elem) =>
+  console.log(elem)
+);
+
+//for of 반복문
+console.log('===========for of 반복문 ===========');
+const pinks = document.querySelectorAll('.pink');
+for (let pink of pinks) {
+  console.log(pink);
+}

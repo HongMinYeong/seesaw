20b68c7a343ea175413dee973f739d75c7e1b830
HongMinYeong <minyung1240@daum.net>
2023-08-26 15:40:35 +0900

    Practice : MVC 실습 진행중

---
+++ 15-mvc-pra/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: true }));

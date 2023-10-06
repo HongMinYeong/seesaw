f8a7fd48ceb6b67ed30274d41a0cc31befefdaa8
HongMinYeong <minyung1240@daum.net>
2023-10-06 21:17:20 +0900

    Study:useEffect 훅 클래스컴포넌트에서 사용하기

--- 27-ref-lifecycle/lifecycle/src/App.js
+++ 27-ref-lifecycle/lifecycle/src/App.js
@@ -1,3 +1,4 @@
+import LifeCycleClass from './LifeCycleClass';
 import LifeCycleFunc from './LifeCycleFunc';
 import logo from './logo.svg';
 
@@ -5,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <LifeCycleFunc />
+      <hr />
+      <LifeCycleClass />
     </div>
   );
 }

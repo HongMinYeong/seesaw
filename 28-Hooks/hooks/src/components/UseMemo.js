4ab5cf7b2d822ac509faff279f635ef82e32b47d
hongminyeong <minyung1240@daum.net>
2023-10-09 16:58:09 +0900

    Study:UseMemo 훅 적용 후

--- 28-Hooks/hooks/src/components/UseMemo.js
+++ 28-Hooks/hooks/src/components/UseMemo.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 export default function UseMemo() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState('');
   //[before]
-  const calc = () => {
-    console.log('열심히 계산 중...');
+  // const calc = () => {
+  //   console.log('열심히 계산 중...');
+  //   for (let i = 0; i < 100000; i++) {}
+  //   return count ** 2;
+  // };
+
+  //[after]
+  const calc = useMemo(() => {
     for (let i = 0; i < 100000; i++) {}
     return count ** 2;
-  };
+  }, [count]);
   return (
     <div>
       <input
@@ -18,7 +24,10 @@ export default function UseMemo() {
       />
       <button onClick={() => setCount(() => count + 1)}>Plus</button>
       <p>count :{count} </p>
-      <p>calc :{calc()} </p>
+      {/* [before] */}
+      {/* <p>calc :{calc()} </p> */}
+      {/* [after] */}
+      <p>calc :{calc} </p>
     </div>
   );
 }
